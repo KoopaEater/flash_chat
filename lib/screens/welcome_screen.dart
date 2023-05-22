daabab4ac9f07da1893d7f17b004951ad7dca112
Max Kørner Andersen <maxkandersen@gmail.com>
2023-05-22 11:23:17 +0200

    Added some animations

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,18 +3,54 @@ import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
-
   static const String id = 'welcome_screen';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = ColorTween(begin: Colors.blueGrey[100], end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    animation.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+
+    animation.addStatusListener((status) {
+      print(status);
+    });
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller.dispose();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
